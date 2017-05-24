5f31e66fe92f1a889ae47f37dbc1d1b74a56b5b3
userniel <dany19971027@gmail.com>
2017-05-24 12:16:40 +0800

    Add files via upload

---
+++ sketch_may24a.ino
@@ -0,0 +1,53 @@
+byte led_digit[8][7] = 
+{
+  { 1,0,0,0,0,0,0 },
+  { 0,1,0,1,0,0,0 },
+  { 0,0,1,0,1,0.0 },
+  { 1,0,0,1,0,1,0 },
+  { 0,1,1,0,0,0,1 },
+  { 0,1,0,0,0,1,0 },
+  { 1,0,0,0,1,0,0 },
+  { 0,1,0,1,0,1,0 }
+};
+
+const int analogInPin = A0;
+const int ledOutPin = 9;
+
+int sensorValue = 0;
+int outputValue = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void Show(int b){
+  byte pin = 7;
+  for(byte led = 0; led < 7; led++){
+    digitalWrite(pin, led_digit[b][led]);
+    pin++;
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensorValue = analogRead(analogInPin);
+  outputValue = map(sensorValue, 0, 1000, 0, 10);
+  
+  for(byte b = 0; b < 8; b++){
+    Show(b);
+    delay(outputValue*20);
+  }
+
+  Serial.print("\nsensor = ");
+  Serial.print(sensorValue);
+  Serial.print(" output = ");
+  Serial.print(outputValue);
+}
